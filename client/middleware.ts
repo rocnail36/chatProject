60d2ff22a4fbb1f46acb4e567c31e6952ec6f2ab
unknown <jdanielrojas18@gmail.com>
2024-08-06 03:12:07 -0400

    l

--- client/middleware.ts
+++ client/middleware.ts
@@ -3,6 +3,13 @@ import { auth } from "@/auth"
 export default auth((req) => {
 
 
+  
+  if(!req.auth && req.nextUrl.pathname.includes("_next/image") ){
+    const newUrl = new URL("/",req.nextUrl.origin)
+    return Response.redirect(newUrl)
+  }
+
+
 
   if (!req.auth && (req.nextUrl.pathname !== "/login" && req.nextUrl.pathname !== "/register"  ) ) {
     const newUrl = new URL("/login", req.nextUrl.origin)
