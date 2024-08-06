88588e4a2f57bddd8cf697791d8473227a74bbad
unknown <jdanielrojas18@gmail.com>
2024-08-06 02:20:50 -0400

    l

--- client/middleware.ts
+++ client/middleware.ts
@@ -4,20 +4,3 @@ export default auth((req) => {
 
 
 
-  if (!req.auth && (req.nextUrl.pathname !== "/login" && req.nextUrl.pathname !== "/register"  ) ) {
-    const newUrl = new URL("/login", req.nextUrl.origin)
-    console.log(newUrl.pathname)
-    return Response.redirect(newUrl)
-  }
-
-
-  if(req.auth && (req.nextUrl.pathname == "/login" || req.nextUrl.pathname == "/register"  )){
-    const newUrl = new URL("/",req.nextUrl.origin)
-    return Response.redirect(newUrl)
-  }
-
-})
-
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
